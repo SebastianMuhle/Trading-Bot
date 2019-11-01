a28299bc61e785f30c629e5261da0a9b0fe33b00
Sebastian Muhle <SebastianMuhle@me.com>
2019-11-01 19:34:35 +0100

    First run of jupyter Notebook works!

--- stock_pred.py
+++ stock_pred.py
@@ -23,7 +23,7 @@ def train_bot(file_path):
     unscaled_y_test = unscaled_y[n:]
 
     model = build_model(history_points, technical_indicators)
-    model.fit(x=[ohlcv_train, tech_ind_train], y=y_train, batch_size=32, epochs=2, shuffle=True, validation_split=0.1, verbose=2)
+    model.fit(x=[ohlcv_train, tech_ind_train], y=y_train, batch_size=32, epochs=50, shuffle=True, validation_split=0.1, verbose=2)
 
     # evaluation
     y_test_predicted = model.predict([ohlcv_test, tech_ind_test])
@@ -36,68 +36,6 @@ def train_bot(file_path):
     scaled_mse = real_mse / (np.max(unscaled_y_test) - np.min(unscaled_y_test)) * 100
     print(scaled_mse)
 
-    return scaled_mse, unscaled_y_test, y_test_predicted, ohlcv_test, tech_ind_test
+    print("Finished training")
 
-
-    buys = []
-    sells = []
-    thresh = 0.1
-
-    start = 0
-    end = -1
-
-    x = -1
-    for ohlcv, ind in zip(ohlcv_test[start: end], tech_ind_test[start: end]):
-        normalised_price_today = ohlcv[-1][0]
-        normalised_price_today = np.array([[normalised_price_today]])
-        price_today = y_normaliser.inverse_transform(normalised_price_today)
-        predicted_price_tomorrow = np.squeeze(y_normaliser.inverse_transform(model.predict([[ohlcv], [ind]])))
-        delta = predicted_price_tomorrow - price_today
-        if delta > thresh:
-            buys.append((x, price_today[0][0]))
-        elif delta < -thresh:
-            sells.append((x, price_today[0][0]))
-        x += 1
-    print(f"buys: {len(buys)}")
-    print(f"sells: {len(sells)}")
-
-
-    def compute_earnings(buys_, sells_):
-        purchase_amt = 10
-        stock = 0
-        balance = 0
-        while len(buys_) > 0 and len(sells_) > 0:
-            if buys_[0][0] < sells_[0][0]:
-                # time to buy $10 worth of stock
-                balance -= purchase_amt
-                stock += purchase_amt / buys_[0][1]
-                buys_.pop(0)
-            else:
-                # time to sell all of our stock
-                balance += stock * sells_[0][1]
-                stock = 0
-                sells_.pop(0)
-        print(f"earnings: ${balance}")
-
-
-    # we create new lists so we dont modify the original
-    compute_earnings([b for b in buys], [s for s in sells])
-
-    import matplotlib.pyplot as plt
-
-    plt.gcf().set_size_inches(22, 15, forward=True)
-
-    real = plt.plot(unscaled_y_test[start:end], label='real')
-    pred = plt.plot(y_test_predicted[start:end], label='predicted')
-
-    if len(buys) > 0:
-        plt.scatter(list(list(zip(*buys))[0]), list(list(zip(*buys))[1]), c='#00ff00', s=50)
-    if len(sells) > 0:
-        plt.scatter(list(list(zip(*sells))[0]), list(list(zip(*sells))[1]), c='#ff0000', s=50)
-
-    # real = plt.plot(unscaled_y[start:end], label='real')
-    # pred = plt.plot(y_predicted[start:end], label='predicted')
-
-    plt.legend(['Real', 'Predicted', 'Buy', 'Sell'])
-
-    plt.show()
+    return scaled_mse, unscaled_y_test, y_test_predicted, ohlcv_test, tech_ind_test, model, y_normaliser
---
+++ trading.py
@@ -0,0 +1,50 @@
+import numpy as np
+np.random.seed(4)
+
+
+def trading(ohlcv_test, tech_ind_test, y_normaliser, model):
+    buys = []
+    sells = []
+    thresh = 0.1
+
+    start = 0
+    end = -1
+
+    x = -1
+    for ohlcv, ind in zip(ohlcv_test[start: end], tech_ind_test[start: end]):
+        normalised_price_today = ohlcv[-1][0]
+        normalised_price_today = np.array([[normalised_price_today]])
+        price_today = y_normaliser.inverse_transform(normalised_price_today)
+        predicted_price_tomorrow = np.squeeze(y_normaliser.inverse_transform(model.predict([[ohlcv], [ind]])))
+        delta = predicted_price_tomorrow - price_today
+        if delta > thresh:
+            buys.append((x, price_today[0][0]))
+        elif delta < -thresh:
+            sells.append((x, price_today[0][0]))
+        x += 1
+    print('The numbers of buys and sells that our bot would have taken')
+    print(f"buys: {len(buys)}")
+    print(f"sells: {len(sells)}")
+
+    def compute_earnings(buys_, sells_):
+        purchase_amt = 10
+        stock = 0
+        balance = 0
+        while len(buys_) > 0 and len(sells_) > 0:
+            if buys_[0][0] < sells_[0][0]:
+                # time to buy $10 worth of stock
+                balance -= purchase_amt
+                stock += purchase_amt / buys_[0][1]
+                buys_.pop(0)
+            else:
+                # time to sell all of our stock
+                balance += stock * sells_[0][1]
+                stock = 0
+                sells_.pop(0)
+        print('The money our bot would have earned')
+        print(f"earnings: ${balance}")
+
+    # we create new lists so we dont modify the original
+    compute_earnings([b for b in buys], [s for s in sells])
+
+    return buys, sells
