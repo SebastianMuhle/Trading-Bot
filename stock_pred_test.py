b637162d41397a18c39bcce8d7026cb0d1a03ffd
Sebastian Muhle <SebastianMuhle@me.com>
2019-11-01 10:20:13 +0100

    Added data preprocessing file and integrated it into stock_pred_test.py and traiding_test.py

--- stock_pred_test.py
+++ stock_pred_test.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 from keras.models import Model
 from keras.layers import Dense, Dropout, LSTM, Input, Activation, concatenate
 from keras import optimizers
+from data_preprocessing import csv_to_dataset
 import numpy as np
 np.random.seed(4)
 
@@ -13,89 +14,8 @@ np.random.seed(4)
 history_points = 50
 
 
-def csv_to_dataset(csv_path):
-    print("Start csv_to_dataset")
-    # Read the csv
-    data = pd.read_csv(csv_path)
-    print(data.head())
-    print(data.shape)
-
-    # Read S&P 500 and calculate the daily change rate
-    stock_index = pd.read_csv('data/SP500.csv')
-    open_price = stock_index['Open']
-    change = (open_price - open_price.shift(1)) / open_price.shift(1) * 100
-    stock_index['change_S&P500'] = change
-    stock_index.dropna(inplace=True)
-    del (stock_index['Open'])
-    del (stock_index['High'])
-    del (stock_index['Low'])
-    del (stock_index['Close'])
-    del (stock_index['Adj Close'])
-    del (stock_index['Volume'])
-
-    print(stock_index.head())
-
-    # Merge
-    #data = pd.merge(data, stock_index, on='timestamp')
-    print("Before reordering")
-    print(data.head())
-    print(data.shape)
-    print("After reordering")
-    data = data.reindex(index=data.index[::-1])
-    print(data.head())
-
-
-    # Drop the timestamp of the data
-    # It is not necessary for the model to know this
-    data = data.drop('timestamp', axis=1)
-    # Drop the first data point
-    # It might contain an IPO which has a high volume that later effects the scaling of the volumen variable
-    data = data.drop(0, axis=0)
-
-    # TODO Necessary?
-    data = data.values
-
-    # Normalise between 0 and 1
-    # This guarantees for nice gradients and thereby faster convergence of the model
-    data_normaliser = preprocessing.MinMaxScaler()
-    data_normalised = data_normaliser.fit_transform(data)
-
-    # TODO replace 'ohlcv_histories_normalised' with 'training window'
-    # TODO and the comment # Containing open high low close volume
-    print('Length data normalised', len(data_normalised))
-    ohlcv_histories_normalised = np.array(
-        [data_normalised[i:i + history_points].copy() for i in range(len(data_normalised) - history_points)])
-    next_day_open_values_normalised = np.array(
-        [data_normalised[:, 0][i + history_points].copy() for i in range(len(data_normalised) - history_points)])
-    next_day_open_values_normalised = np.expand_dims(next_day_open_values_normalised, -1)
-
-    print('Shape ohlcv', ohlcv_histories_normalised.shape)
-    # Used for plotting later
-    next_day_open_values = np.array([data[:, 0][i + history_points].copy() for i in range(len(data) - history_points)])
-    next_day_open_values = np.expand_dims(next_day_open_values, -1)
-
-    # Used to unnormalized the predicted values
-    y_normaliser = preprocessing.MinMaxScaler()
-    y_normaliser.fit(next_day_open_values)
-
-    technical_indicators = []
-    for his in ohlcv_histories_normalised:
-        # note since we are using his[3] we are taking the SMA of the closing price
-        sma = np.mean(his[:, 3])
-        technical_indicators.append(np.array([sma]))
-        # technical_indicators.append(np.array([sma,macd,]))
-
-    technical_indicators = np.array(technical_indicators)
-
-    tech_ind_scaler = preprocessing.MinMaxScaler()
-    technical_indicators_normalised = tech_ind_scaler.fit_transform(technical_indicators)
-
-    assert ohlcv_histories_normalised.shape[0] == next_day_open_values_normalised.shape[0] == \
-           technical_indicators_normalised.shape[0]
-    return ohlcv_histories_normalised, technical_indicators_normalised, next_day_open_values_normalised, next_day_open_values, y_normaliser
-
 # Get the data
-ohlcv_histories, technical_indicators, next_day_open_values, unscaled_y, y_normaliser = csv_to_dataset('data/MSFT_daily.csv')
+ohlcv_histories, technical_indicators, next_day_open_values, unscaled_y, y_normaliser = csv_to_dataset('data/daily_MSFT.csv', history_points)
 
 # Train-Test Set split
 test_split = 0.9
@@ -141,7 +61,7 @@ z = Dense(1, activation="linear", name='dense_out')(z)
 model = Model(inputs=[lstm_branch.input, technical_indicators_branch.input], outputs=z)
 adam = optimizers.Adam(lr=0.0005)
 model.compile(optimizer=adam, loss='mse')
-model.fit(x=[ohlcv_train, tech_ind_train], y=y_train, batch_size=32, epochs=50, shuffle=True, validation_split=0.1, verbose=2)
+model.fit(x=[ohlcv_train, tech_ind_train], y=y_train, batch_size=32, epochs=2, shuffle=True, validation_split=0.1, verbose=2)
 # evaluation
 
 y_test_predicted = model.predict([ohlcv_test, tech_ind_test])
--- trading_test.py
+++ trading_test.py
@@ -4,7 +4,7 @@ from stock_pred_test import csv_to_dataset, history_points
 
 model = load_model('technical_model.h5')
 
-ohlcv_histories, technical_indicators, next_day_open_values, unscaled_y, y_normaliser = csv_to_dataset('MSFT_daily.csv')
+ohlcv_histories, technical_indicators, next_day_open_values, unscaled_y, y_normaliser = csv_to_dataset('data/daily_MSFT.csv', 50)
 
 test_split = 0.9
 n = int(ohlcv_histories.shape[0] * test_split)
